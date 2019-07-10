05753244f948a6b112cd03f67847131fd65988da
Catsudemo <cattmott@gmail.com>
2019-07-10 15:05:09 +0200

    Pokemon homework
    Adding pokemon homework to portfoli

--- docs/hyf-javascript2/week-3/homework/step2-1.js
+++ docs/hyf-javascript2/week-3/homework/step2-1.js
@@ -1,9 +1,7 @@
 'use strict';
 
 function foo(func) {
-  // What to do here?
-  // Replace this comment and the next line with your code
-  console.log(func);
+  if (func) console.log(func);
 }
 
 function bar() {
